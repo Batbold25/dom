512ab7cc028f2dd1fee742d2e25cb74b33d3da81
Батболд Гансүх <23LP0812@G304-CC0087.local>
2023-11-15 19:22:02 +0800

    shine hicheel

---
+++ dom.js
@@ -0,0 +1,13 @@
+// const btm = document.getElementById("change")
+// console.log(btm);
+// let i = 0;
+// function change() {
+//     const box = document.createElement("div");
+//     i++;
+//     box.classList.add("box")
+//     box.innerText = i;
+
+//     document.body.appendChild(box)
+// }
+
+// btm.addEventListener("click",change)
